cbc9400145aa8bbe5181ab55fdaf3ba38ca5dc58
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2024-05-04 16:27:06 +0700

    add json

--- dataset/to_json.py
+++ dataset/to_json.py
@@ -4,8 +4,14 @@ df = pd.read_csv("data_repo_jtik.csv")
 df = df[df['nama_pembimbing'] != '-']
 df['kompetensi'] = ""
 
-counts = df["prodi"].value_counts().sort_index()
-print(len(df))
-print(counts)
+def generate_id_sequence_prodi(group):
+    group['id_sequence_prodi'] = range(1, len(group) + 1)
+    return group
 
+df = df.groupby('prodi').apply(generate_id_sequence_prodi)
+
+print(len(df))
+print(df["prodi"].value_counts().sort_index())
+
+# Save the dataframe to JSON
 df.to_json("init_data_repo_jtik.json", orient="records")
