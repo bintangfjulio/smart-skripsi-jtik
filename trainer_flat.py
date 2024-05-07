84ec816e266e11de292f8355cfa4aec718677859
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2024-05-07 13:29:59 +0700

    add hierarchy

--- trainer_flat.py
+++ trainer_flat.py
@@ -27,7 +27,6 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 pd.options.display.float_format = '{:,.2f}'.format  
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--target", choices=['nama_pembimbing', 'kompetensi', 'prodi'], required=True, help='Target Column Name')
 parser.add_argument("--dataset", type=str, default='init_data_repo_jtik.json', help='Dataset Path')
 parser.add_argument("--batch_size", type=int, default=32, help='Batch Size')
 parser.add_argument("--bert_model", type=str, default="indolem/indobert-base-uncased", help='BERT Model API')
@@ -41,6 +40,7 @@ parser.add_argument("--in_channels", type=int, default=4, help='CNN In Channels'
 parser.add_argument("--out_channels", type=int, default=32, help='CNN Out Channels')
 parser.add_argument("--window_sizes", nargs="+", type=int, default=[1, 2, 3, 4, 5], help='CNN Kernel')
 parser.add_argument("--num_bert_states", type=int, default=4, help='Number of BERT Last States')
+parser.add_argument("--target", choices=['nama_pembimbing', 'kompetensi', 'prodi'], required=True, help='Target Column Name')
 config = vars(parser.parse_args())
 
 np.random.seed(config["seed"]) 
@@ -52,14 +52,17 @@ if torch.cuda.is_available():
     torch.cuda.manual_seed_all(config["seed"])
     torch.backends.cudnn.deterministic = True
 
-dataset = pd.read_csv(f'dataset/{config["dataset"]}')
+dataset = pd.read_json(f'dataset/{config["dataset"]}')
 dataset = dataset[dataset['nama_pembimbing'] != '-']
 
 stop_words = StopWordRemoverFactory().get_stop_words()
 tokenizer = BertTokenizer.from_pretrained(config["bert_model"], use_fast=False)
 stemmer = StemmerFactory().create_stemmer()
-labels = sorted(dataset[config["target"]].unique().tolist())
 pretrained_bert = BertModel.from_pretrained(config["bert_model"], output_attentions=False, output_hidden_states=True)
+
+
+# Flat Target
+labels = sorted(dataset[config["target"]].unique().tolist())
 
 
 # preprocessor
