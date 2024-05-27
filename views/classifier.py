67983641120f329308adfbbb1a2382d6701ff2ba
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2024-05-27 12:15:43 +0700

    backup

--- views/classifier.py
+++ views/classifier.py
@@ -1,6 +1,6 @@
 import jsonify
 
-from flask import Blueprint, request, redirect, url_for, flash
+from flask import Blueprint
 from middleware import role_required
 
 
@@ -9,17 +9,4 @@ classifier = Blueprint('classifier', __name__, template_folder='templates', url_
 @classifier.route('/inference', methods=['POST'])
 @role_required('pengguna')
 def inference():
-    id = request.form['id']
-    keywords = request.form['keywords']
-    abstrak = request.form['abstrak']
-
-    if not keywords or not abstrak:
-        flash(('Klasifikasi Gagal', 'Seluruh isian harus diisi'), 'error')
-    
-    try:
-        text = keywords + " - " + abstrak
-
-    except Exception as e:
-        flash(('Klasifikasi Gagal', 'Terjadi kesalahan server saat klasifikasi'), 'error')
-        
-    return redirect(url_for('dashboard.classifier'))
+    pass
